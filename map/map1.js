19ee1260f6ef1f28cd3f0019724e7330222833fe
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-10-06 13:19:31 -0400

    replace camera (10/6/23)

--- map/map1.js
+++ map/map1.js
@@ -1,7 +1,7 @@
 export const createScene = () => {
     const scene = new BABYLON.Scene(engine);
 
-    const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
+    const camera = new BABYLON.UniversalCamera("myCamera", new BABYLON.Vector3(0, 0, 0))
     camera.attachControl(canvas, true);
 
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
