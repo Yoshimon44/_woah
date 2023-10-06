36fe83644cbeb84ff0fb252f409e49d4a4330b86
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-10-06 12:59:00 -0400

    doodoo balls

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ window.onload = function(){
     var canvas = document.querySelector("#newCanva");
     var engine = new BABYLON.Engine(canvas, true);
 
-    console.log(engine);
+    function createScene(){
+
+    }
+
+    var scene = createScene();
+    window.addEventListener("resize", function () {
+        engine.resize();
+    });
 }
