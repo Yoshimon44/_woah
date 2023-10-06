4c4457682ad2e252abd72781a00c2856620e00f7
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-10-06 13:10:42 -0400

    make map1 a module

--- map/map1.js
+++ map/map1.js
@@ -1,4 +1,4 @@
-const createScene = () => {
+export const createScene = () => {
     const scene = new BABYLON.Scene(engine);
 
     const camera = new BABYLON.ArcRotateCamera("camera", -Math.PI / 2, Math.PI / 2.5, 3, new BABYLON.Vector3(0, 0, 0));
