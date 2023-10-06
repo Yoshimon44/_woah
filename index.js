e218a33377cc37d8890014da39c853f5ccf98383
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-10-06 13:03:30 -0400

    undecided (10/6/23)

--- index.js
+++ index.js
@@ -2,11 +2,6 @@ window.onload = function(){
     var canvas = document.querySelector("#newCanva");
     var engine = new BABYLON.Engine(canvas, true);
 
-    function createScene(){
-
-    }
-
-    var scene = createScene();
     window.addEventListener("resize", function () {
         engine.resize();
     });
