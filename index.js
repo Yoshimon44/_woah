2cfe25906c2df0fe6f404f0d6861b96c759fb07b
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-10-06 13:22:21 -0400

    currentScene() in index.js?....

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
+var e = await import("/map/map1.js");
+
 window.onload = function(){
     var canvas = document.querySelector("#newCanva");
     var engine = new BABYLON.Engine(canvas, true);
+
+    e.createScene();
 
     window.addEventListener("resize", function () {
         engine.resize();
