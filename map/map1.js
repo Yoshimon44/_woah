50951344dabf0b7a86b9ad74acdf3f620f07c02f
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-10-06 21:31:31 -0400

    adding testing tools (10/6/23);

--- map/map1.js
+++ map/map1.js
@@ -1,12 +1,18 @@
+export var mapParts = {}; //for testing purposes
+
 export const createScene = (canvas, engine) => {
     const scene = new BABYLON.Scene(engine);
 
     const camera = new BABYLON.UniversalCamera("myCamera", new BABYLON.Vector3(0, 0, 0))
+    mapParts.camera = camera;
     camera.attachControl(canvas, true);
 
+
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
+    mapParts.light = light;
 
     const box = BABYLON.MeshBuilder.CreateBox("box", {});
+    mapParts.box = box;
 
     camera.setTarget(box.position);
 
