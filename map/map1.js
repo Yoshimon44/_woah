ca377ca03a8bad5b5954c68375205bb1f75e107f
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-10-11 11:27:16 -0400

    improving the testing map (10/11/23)

--- map/map1.js
+++ map/map1.js
@@ -1,4 +1,5 @@
-export var mapParts = {};
+export var mapParts = {}; //these are not 'map parts' 😭😭😭
+export var mapPartsReal = {};
 
 export const createScene = (canvas, engine) => {
     const scene = new BABYLON.Scene(engine);
@@ -19,23 +20,29 @@ export const createScene = (canvas, engine) => {
         // [mouseCamera.js]
         console.log(engine);
         engine.enterPointerlock();
-        //map1.mapParts.engine.isPointerLock = true;
-        //console.log(document.querySelector("#newCanva"));
-        //document.querySelector("#newCanva").focus();
     })
 
     const light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0));
     mapParts.light = light;
-    try {
-        light.intensity = 1;
-    } catch (err) {
-        console.log(err);
-    }
+    light.intensity = 1;
+
+    // [MAP]
 
     const box = BABYLON.MeshBuilder.CreateBox("box", {width: 10, height: 10});
-    mapParts.box = box;
+    mapPartsReal.box = box;
+
+    const box2 = BABYLON.MeshBuilder.CreateBox("box2", {width: 10, height: 10});
+    box2.position = new BABYLON.Vector3(-6, 5, -9)
+    mapPartsReal.box2 = box2;
+
+    const box3 = BABYLON.MeshBuilder.CreateBox("box2", {width: 10, height: 10});
+    box3.position = new BABYLON.Vector3(7, 2, 7)
+    mapPartsReal.box3 = box3;
 
     camera.setTarget(box._position);
+
+    const ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 50, width : 50});
+    mapPartsReal.ground = ground;
 
 
     return scene;
