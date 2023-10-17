3a4aa984a0383e6c9834db7d2f911b91a26da5eb
lmaopalbol <97364497+lmaopalbol@users.noreply.github.com>
2023-10-17 09:34:28 -0400

    attempting to make collision more accurate (10/17/23)

--- map/map1.js
+++ map/map1.js
@@ -13,7 +13,7 @@ export const createScene = (canvas, engine) => {
     camera.isMovingZ = false;
     camera.isMovingX = false;
     
-    camera.ellipsoid = new BABYLON.Vector3(1.3,1.3,1.3);
+    camera.ellipsoid = new BABYLON.Vector3(1.4,1.4,1.4);
     mapParts.camera = camera;
     camera.attachControl(canvas, true);
 
