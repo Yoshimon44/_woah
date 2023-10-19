c12d6713a11f8874b536edf40d5822921cdbf518
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-10-19 11:35:20 -0400

    raycast

--- map/keyboardE.js
+++ map/keyboardE.js
@@ -223,14 +223,22 @@ export function main(){
       var x = camera.rotation.x*(deg)
       var y = camera.rotation.y*(deg)
       var z = camera.rotation.z*(deg)
+
+
+
       var forwardsVector = new BABYLON.Vector3(
         Math.sin(camera.rotation.y),
         0, //-Math.sin(camera.rotation.x)
         -Math.cos(camera.rotation.y - Math.PI)
       );
       console.log(x,y,z)
+
+
+
       var raycastResult = new BABYLON.PhysicsRaycastResult();
-      var start = new BABYLON.Vector3(x, y, z);
+      //var start = new BABYLON.Vector3(x, y, z);
+
+      var start = camera.position;
       var end = start.add(forwardsVector.scale(900));
       physicsEngine.raycastToRef(start, end, raycastResult);
 
