1686af5288dcdc281bab3a3a29f83a0a4b668d4e
lmaopalbol <97364497+lmaopalbol@users.noreply.github.com>
2023-10-20 11:17:01 -0400

    needMoveGravity fix

--- map/keyboardE.js
+++ map/keyboardE.js
@@ -114,7 +114,7 @@ export function main(){
   camera.keysLeft = [65];
   camera.keysRight = [68];
 
-  camera.needMoveForGravity(true);
+  camera.needMoveForGravity = true;
 
   window.addEventListener("keydown", function(e){
 
