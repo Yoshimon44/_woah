8c5866e1c8b8e1156d1fa0aa4525adcdef0001c8
lmaopalbol <97364497+lmaopalbol@users.noreply.github.com>
2023-10-24 08:51:04 -0400

    damage spread fix

--- map/keyboardE.js
+++ map/keyboardE.js
@@ -35,7 +35,7 @@ var nearestPartMagnitude; //used in getNearestPart.
 
 // [HELPER FUNCTIONS]
 function randomInt(min, max){
-  return Math.floor(Math.random()) * (max-min+1) + min;
+  return Math.floor(Math.random() * (max-min+1))  + min;
 }
 
 function updateLabel(label, value) {  
