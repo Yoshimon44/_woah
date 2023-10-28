0b6d093bcf06ab9b8ed2767c7ccc1f6dddc2fd93
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-10-27 22:52:12 -0400

    enemy? (10/27/23) (1.1)

--- map/enemy_AI.js
+++ map/enemy_AI.js
@@ -8,7 +8,7 @@ export class Enemy { //may not be the system we will use in the deliverable
 
 export class TestDummy {
     constructor(){
-        this.character = BABYLON.MeshBuilder.CreateMesh('box', {width: 5, height: 5, depth: 2});
+        this.character = BABYLON.MeshBuilder.CreateBox('enemy' + Date.now().toString(), {width: 5, height: 5, depth: 2});
         this.character.position = new BABYLON.Vector3(-10, 1, 10);
         this.character.billboardMode = 2;
 
