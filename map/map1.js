c923b1fec1d33202baab09aaeabf8acae489eaae
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-10-31 21:58:11 -0400

    the enemy is too fast 😭😭😭

--- map/map1.js
+++ map/map1.js
@@ -41,9 +41,9 @@ export class TestDummy {
 
         var walkLoop = setInterval(function(){
             walkingCharacter.position.addInPlace(
-                point.subtract(startPosition).normalize().scale(walkingCharacter.walkSpeed)
+                point.subtract(startPosition).normalize().scale(walkingCharacter.walkSpeed/10)
             );
-            console.log(point.subtract(startPosition).normalize().scale(walkingCharacter.walkSpeed))
+            console.log(point.subtract(startPosition).normalize().scale(walkingCharacter.walkSpeed/10))
             console.log(BABYLON.Vector3.Distance(startPosition, walkingCharacter.position))
             console.log(BABYLON.Vector3.Distance(startPosition, point))
 
