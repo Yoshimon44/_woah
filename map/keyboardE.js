faae0e36c5d3c57d935c2e591eb4d0d7ec84e55f
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-11-03 09:48:00 -0400

    ay yeah i been braggin

--- map/keyboardE.js
+++ map/keyboardE.js
@@ -174,10 +174,11 @@ function filePathParser(hecknaw){
 }
 
 export function makeStair(startPos, endPos, steps, stairLength) { //i dont like messy code soooooo
-  var stairDistance = ((endPos.add(startPos)).divide(steps)); //js doesnt support operator overloading so sad man
+  var stairDistance = ((endPos.subtract(startPos)).divide(steps)); //js doesnt support operator overloading so sad man
 
   for (var i = 0; i < steps; i++) {
       var stairstep = BABYLON.MeshBuilder.CreateBox(("stair" + Date.now().toString()), {length: stairLength, width: stairDistance});
+      stairstep.position = startPos.add(stairDistance.scale(i/steps));
   }
       
 
