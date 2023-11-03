257e0fd691d71e90965d73918be719bb1b35f198
Yoshimon44 <146379712+Yoshimon44@users.noreply.github.com>
2023-11-03 18:15:30 -0400

    my sister wants to fall down the stairs

--- map/map1.js
+++ map/map1.js
@@ -92,7 +92,7 @@ export function makeStair(startPos, endPos, steps, stairLength) { //i dont like 
         console.log(stairLength);
         console.log(stairDistance);
         var stairstep = BABYLON.MeshBuilder.CreateBox("stair" + i.toString());
-        stairstep.position = startPos.add(stairDistance.scale(i/steps));
+        stairstep.position = startPos.add(stairDistance * i/steps);
 
         //{width: stairLength, height: stairDistance}
         stairstep.scaling = new BABYLON.Vector3(stairLength, stairDistance, 1);
